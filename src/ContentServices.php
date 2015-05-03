db65ac53f4c98d6d4e1452cc243e678703c74b35
Chris Pliakas <cpliakas@266779.no-reply.drupal.org>
2015-05-03 12:30:14 -0400

    Added ping(), removed ApiProblem library, cleaned up docblocks

--- src/ContentServices.php
+++ src/ContentServices.php
@@ -4,8 +4,6 @@ namespace Acquia\ContentServicesClient;
 
 use Acquia\Hmac\Digest as Digest;
 use GuzzleHttp\Client;
-use GuzzleHttp\Exception\RequestException;
-use Crell\ApiProblem\ApiProblem;
 use Acquia\Hmac\RequestSigner;
 use Acquia\Hmac\Guzzle5\HmacAuthPlugin;
 
@@ -35,8 +33,9 @@ class ContentServices extends Client
     }
 
     /**
-     * @param array $config
-     * @return static
+     * @param  array                                         $config
+     *
+     * @return \Acquia\ContentServicesClient\ContentServices
      */
     public static function factory($config = array())
     {
@@ -55,109 +54,98 @@ class ContentServices extends Client
     }
 
     /**
+     * Pings the service to ensure that it is available.
      *
-     * @param \Acquia\ContentServicesClient\Entity $entity
+     * @return \GuzzleHttp\Message\Response
      *
-     * @return ContentServicesException
+     * @throws \GuzzleHttp\Exception\RequestException
+     *
+     * @since 0.2.0
+     */
+    public function ping()
+    {
+        return $this->get('/ping');
+    }
+
+    /**
+     * Sends entity creation request to Plexus.
+     *
+     * @param  \Acquia\ContentServicesClient\Entity   $entity
+     *
+     * @return \GuzzleHttp\Message\Response
+     *
+     * @throws \GuzzleHttp\Exception\RequestException
      */
     public function createEntity(Entity $entity)
     {
-        try {
-            $request = $this->createRequest('POST', '/entities', ['json' => (array) $entity]);
-            $response = $this->send($request);
-            $entity->exchangeArray($response->json());
-
-            return $response;
-        } catch (RequestException $ex) {
-            $this->throwException($ex);
-        }
+        $request = $this->createRequest('POST', '/entities', ['json' => (array) $entity]);
+        $response = $this->send($request);
+        $entity->exchangeArray($response->json());
+        return $response;
     }
 
     /**
-     * @param string $uuid
+     * Returns an entity by UUID.
      *
-     * @return ContentServicesException
+     * @param  string                               $uuid
+     *
+     * @return \Acquia\ContentServicesClient\Entity
+     *
+     * @throws \GuzzleHttp\Exception\RequestException
      */
     public function readEntity($uuid)
     {
-        try {
-            $response = $this->get('entities/'.$uuid);
-            return new Entity($response->json());
-        } catch (\GuzzleHttp\Exception\ClientException $ex) {
-            return new Entity($ex->getResponse()->json());
-        } catch (\GuzzleHttp\Exception\ServerErrorResponseException $ex) {
-            return new Entity($ex->getResponse()->json());
-        } catch (\GuzzleHttp\Exception\BadResponseException $ex) {
-            return new Entity($ex->getResponse()->json());
-        }
+        $response = $this->get('entities/' . $uuid);
+        return new Entity($response->json());
     }
 
     /**
-     * @param \Acquia\ContentServicesClient\Entity $entity
-     * @param string                               $uuid
+     * Updates the passed entity.
      *
-     * @return ContentServicesException
+     * @param  \Acquia\ContentServicesClient\Entity   $entity
+     * @param  string                                 $uuid
+     *
+     * @return \GuzzleHttp\Message\Response
+     *
+     * @throws \GuzzleHttp\Exception\RequestException
      */
     public function updateEntity(Entity $entity, $uuid)
     {
-        try {
-            $request = $this->createRequest('PUT', '/entities/'.$uuid, ['json' => (array) $entity]);
-            $response = $this->send($request);
-            $entity->exchangeArray($response->json());
-
-            return $response;
-        } catch (RequestException $ex) {
-            $this->throwException($ex);
-        }
+        $request = $this->createRequest('PUT', '/entities/'.$uuid, ['json' => (array) $entity]);
+        $response = $this->send($request);
+        $entity->exchangeArray($response->json());
+        return $response;
     }
 
     /**
-     * @param string $uuid
+     * Deletes an entity by UUID.
      *
-     * @return ContentServicesException
+     * @param  string                                 $uuid
+     *
+     * @return \GuzzleHttp\Message\Response
+     *
+     * @throws \GuzzleHttp\Exception\RequestException
      */
     public function deleteEntity($uuid)
     {
-        try {
-            return $this->delete('entities/'.$uuid);
-        } catch (RequestException $ex) {
-            $this->throwException($ex);
-        }
+        return $this->delete('entities/' . $uuid);
     }
 
     /**
-     * @param $index
-     * @param $query
-     * @return mixed
+     * Search for entities.
+     *
+     * @param  string                                 $index
+     * @param  array                                  $query
+     *
+     * @return array
+     *
+     * @throws \GuzzleHttp\Exception\RequestException
      */
     public function searchEntity($index, $query)
     {
         $url = '/elastic/' . $index . '/_search';
-
-        try {
-            $request = $this->createRequest('POST', $url, ['json' => (array) $query]);
-            $response = $this->send($request);
-            $json = $response->json();
-            return $json;
-        } catch (RequestException $ex) {
-            $this->throwException($ex);
-        }
-    }
-
-    /**
-     * @param  RequestException         $ex
-     * @throws ContentServicesException
-     */
-    public function throwException(RequestException $ex)
-    {
-        if ($ex->hasResponse()) {
-            $response = $ex->getResponse();
-            $problem = ApiProblem::fromJson($response->getBody());
-            $problem->setStatus($response->getStatusCode());
-            throw new ContentServicesException($problem, $ex);
-        } else {
-            $problem = new ApiProblem('Unknown error occured');
-            throw new ContentServicesException($problem, $ex);
-        }
+        $request = $this->createRequest('POST', $url, ['json' => (array) $query]);
+        $response = $this->send($request);
+        return $response->json();
     }
 }
