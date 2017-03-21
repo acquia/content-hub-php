c129568b777e395911369b76f49a9247b1ae09c2
Alejandro Barrios <abarrios@ieee.org>
2017-03-20 20:57:26 -0400

    LAT-6 | Change format in Text fields from basic html to filtered_html

--- src/ContentHub.php
+++ src/ContentHub.php
@@ -213,8 +213,29 @@ class ContentHub extends Client
         unset($data['attributes']['langcode']);
 
         foreach ($data['attributes'] as $attribute_name => $attribute_value) {
-          $data['attributes'][$attribute_name]['value'][$this->to_langcode] = $data['attributes'][$attribute_name]['value'][$from_lancode];
-          unset($data['attributes'][$attribute_name]['value'][$from_lancode]);
+            $data['attributes'][$attribute_name]['value'][$this->to_langcode] = $data['attributes'][$attribute_name]['value'][$from_lancode];
+            unset($data['attributes'][$attribute_name]['value'][$from_lancode]);
+
+            if ($attribute_value['type'] == 'array<string>') {
+                foreach ($data['attributes'][$attribute_name]['value'][$this->to_langcode] as $key => $item) {
+                    $field_value = json_decode($item, TRUE);
+                    if (is_array($field_value) && isset($field_value['format'])) {
+
+                        switch ($field_value['format']) {
+                            case 'basic html':
+                                $field_value['format'] = 'filtered_html';
+                                break;
+
+                            case 'rich_text':
+                                $field_value['format'] = 'full_html';
+                                break;
+
+                        }
+
+                        $data['attributes'][$attribute_name]['value'][$this->to_langcode][$key] = json_encode($field_value, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT);
+                    }
+                }
+            }
         }
         
         return $data;
