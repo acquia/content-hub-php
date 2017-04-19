2be8debda0137047b67fe2e16ae7fe6c10b75c54
Yuan Xie <yuan.xie@acquia.com>
2017-04-19 17:59:07 -0400

    LAT-362 | Add comments for exceptions.

--- src/Data/Exception/UnsupportedFormatException.php
+++ src/Data/Exception/UnsupportedFormatException.php
@@ -2,4 +2,9 @@
 
 namespace Acquia\ContentHubClient\Data\Exception;
 
+/**
+ * Class UnsupportedFormatException.
+ *
+ * @package Acquia\ContentHubClient\Data\Exception
+ */
 class UnsupportedFormatException extends \Exception {}
