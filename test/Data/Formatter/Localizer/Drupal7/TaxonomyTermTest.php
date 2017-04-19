2da9cc71a28643a0fbe93cffe02e637a84904319
Yuan Xie <yuan.xie@acquia.com>
2017-04-19 01:18:47 -0400

    LAT-362 | Add entity data localizer tests.

---
+++ test/Data/Formatter/Localizer/Drupal7/TaxonomyTermTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Acquia\ContentHubClient\test\Data\Formatter\Localizer;
+
+use Acquia\ContentHubClient\Data\Formatter\Localizer\Entity\Drupal7\TaxonomyTerm as Localizer;
+
+/**
+ * Taxonomy term data localizer test.
+ *
+ * @coversDefaultClass Acquia\ContentHubClient\Data\Formatter\Localizer\Entity\Drupal7\TaxonomyTerm
+ * @group content-hub-php
+ */
+class TaxonomyTermTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Tests the localizeEntity() method.
+     *
+     * @covers ::localizeEntity
+     */
+    public function testLocalizeEntity()
+    {
+        $localizer = new Localizer();
+        $data = [
+            'attributes' => [
+                'vocabulary' => ['vocabulary data'],
+                'name' => [
+                    'type' => 'old_type',
+                    'value' => [
+                        'und' => [
+                            'data1',
+                            'data2',
+                        ],
+                    ],
+                ],
+                'weight' => [
+                    'type' => 'old_type',
+                    'value' => [
+                        'und' => [
+                            'data1',
+                            'data2',
+                        ],
+                    ],
+                ],
+                'description' => [
+                    'type' => 'old_type',
+                    'value' => [
+                        'und' => [
+                            'data1',
+                            'data2',
+                        ],
+                    ],
+                ],
+            ],
+        ];
+        $localizer->localizeEntity($data);
+
+        $expected = [
+            'attributes' => [
+                'vocabulary' => ['vocabulary data'],
+                'name' => [
+                    'type' => 'string',
+                    'value' => [
+                        'und' => 'data1',
+                    ],
+                ],
+                'weight' => [
+                    'type' => 'string',
+                    'value' => [
+                        'und' => 'data1',
+                    ],
+                ],
+                'description' => [
+                    'type' => 'string',
+                    'value' => [
+                        'und' => 'data1',
+                    ],
+                ],
+                'parent' => [
+                    'type' => 'array<reference>',
+                    'value' => [
+                        'und' => [],
+                    ],
+                ],
+                'type' => ['vocabulary data'],
+            ],
+        ];
+        $this->assertEquals($expected, $data);
+    }
+
+}
