eb7258e528eee8f9b7c8a32933a3598988efc3ff
Alejandro Barrios <abarrios@ieee.org>
2017-05-03 16:16:44 -0400

    LAT-35 | Allows to print a list of nodes by using listEntities() method

--- src/Data/Formatter/Localizer/Entity/Drupal7/Node.php
+++ src/Data/Formatter/Localizer/Entity/Drupal7/Node.php
@@ -5,4 +5,21 @@ namespace Acquia\ContentHubClient\Data\Formatter\Localizer\Entity\Drupal7;
 /**
  * Node entity data localizer class.
  */
-class Node extends Entity {}
+class Node extends Entity {
+
+  /**
+   * Localize "listEntities".
+   *
+   * @param mixed $data Data
+   */
+  public function localizeListEntities(&$data)
+  {
+    if (!isset($data['attributes']['title'])) {
+      return;
+    }
+
+    foreach ($data['attributes']['title'] as $language => $value) {
+      $data['attributes']['title'][$language] = is_array($value) ? reset($value) : $value;
+    }
+  }
+}
