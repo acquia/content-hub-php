a699687e07d652f3d4b8e2f9c6fd4e5a986e7d5e
Alejandro Barrios <abarrios@ieee.org>
2018-09-11 16:37:20 -0400

    LCH-2145 | Adding readEntities() method

--- src/ContentHub.php
+++ src/ContentHub.php
@@ -215,6 +215,50 @@ class ContentHub extends Client
     }
 
     /**
+     * Obtains a group of entities, given UUIDs.
+     *
+     * @param array $uuids
+     *   An array of UUIDs.
+     *
+     * @return \Acquia\ContentHubClient\Entity[]
+     *   An array of Content Hub Entity objects.
+     */
+    public function readEntities($uuids)
+    {
+        $url = "/{$this->api_version}/_search";
+        $chunks = array_chunk($uuids, 50);
+        $objects = [];
+        foreach ($chunks as $chunk) {
+            $query = [
+                'size' => 50,
+                'query' => [
+                    'constant_score' => [
+                        'filter' => [
+                            'terms' => [
+                                'uuid' => $chunk,
+                            ],
+                        ],
+                    ],
+                ],
+            ];
+            $body = json_encode((array) $query);
+            $request = new Request('GET', $url, [], $body);
+            $results = $this->getResponseJson($request);
+            if (is_array($results) && isset($results['hits']['total']) && $results['hits']['total'] > 0) {
+                foreach ($results['hits']['hits'] as $key => $item) {
+                    $entity = $item['_source']['data'];
+                    $config = [
+                        'dataType' => 'Entity',
+                    ];
+                    $translatedData = $this->adapter->translate($entity, $config);
+                    $objects[$entity['uuid']] = new Entity($translatedData);
+                }
+            }
+        }
+        return $objects;
+    }
+
+    /**
      * Updates an entity asynchronously.
      *
      * The entity does not need to be passed to this method, but only the resource URL.
