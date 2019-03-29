4ab0753948b07ea8c00af4c258e9e01c7f70c0f9
Jakob Perry <japerry007@gmail.com>
2019-03-28 22:43:36 -0700

    LCH-2618: Update ClientCDF object to properly parse remote data

--- src/CDF/CDFObject.php
+++ src/CDF/CDFObject.php
@@ -70,22 +70,22 @@ class CDFObject implements CDFObjectInterface
      */
     protected $processed = false;
 
-  /**
-   * CDFObject constructor.
-   *
-   * @param string $type
-   *   Object type.
-   * @param string $uuid
-   *   Object UUID.
-   * @param string $created
-   *   Created date.
-   * @param string $modified
-   *   Modified date.
-   * @param string $origin
-   *   Origin UUID.
-   * @param array $metadata
-   *   Object metadata.
-   */
+    /**
+     * CDFObject constructor.
+     *
+     * @param string $type
+     *   Object type.
+     * @param string $uuid
+     *   Object UUID.
+     * @param string $created
+     *   Created date.
+     * @param string $modified
+     *   Modified date.
+     * @param string $origin
+     *   Origin UUID.
+     * @param array $metadata
+     *   Object metadata.
+     */
     public function __construct($type, $uuid, $created, $modified, $origin, $metadata = [])
     {
         $this->type = $type;
@@ -94,6 +94,47 @@ class CDFObject implements CDFObjectInterface
         $this->modified = $modified;
         $this->origin = $origin;
         $this->setMetadata($metadata);
+    }
+
+    /**
+     * Static Factory method to allow CDFObject to interpret their own data.
+     *
+     * @param array $data
+     *
+     * @return \Acquia\ContentHubClient\CDF\CDFObject
+     * @throws \ReflectionException
+     */
+    public static function fromArray(array $data)
+    {
+      $object = new static($data['type'], $data['uuid'], $data['created'], $data['modified'], $data['origin'], $data['metadata']);
+      foreach ($data['attributes'] as $attribute_name => $values) {
+        if (!$attribute = $object->getAttribute($attribute_name)) {
+          $class = !empty($object->getMetadata()['attributes'][$attribute_name]) ? $object->getMetadata()['attributes'][$attribute_name]['class'] : false;
+          if ($class && class_exists($class)) {
+            $object->addAttribute($attribute_name, $values['type'], null, CDFObject::LANGUAGE_UNDETERMINED, $class);
+          } else {
+            $object->addAttribute($attribute_name, $values['type'], null);
+          }
+          $attribute = $object->getAttribute($attribute_name);
+        }
+        $value_property = (new \ReflectionClass($attribute))->getProperty('value');
+        $value_property->setAccessible(true);
+        $value_property->setValue($attribute, $values['value']);
+      }
+      return $object;
+    }
+
+    /**
+     * Static Factory method to format data from JSON into the CDFObject.
+     *
+     * @param string $json
+     *
+     * @return \Acquia\ContentHubClient\CDF\CDFObject
+     * @throws \ReflectionException
+     */
+    public static function fromJson(string $json)
+    {
+      return self::fromArray(json_decode($json, TRUE));
     }
 
     /**
--- src/EventSubscriber/DefaultCDF.php
+++ src/EventSubscriber/DefaultCDF.php
@@ -34,25 +34,7 @@ class DefaultCDF implements EventSubscriberInterface
      */
     public function onGetCDFType(GetCDFTypeEvent $event)
     {
-        $data = $event->getData();
-
-        $object = new CDFObject($data['type'], $data['uuid'], $data['created'], $data['modified'], $data['origin'], $data['metadata']);
-
-        foreach ($data['attributes'] as $attribute_name => $values) {
-            if (!$attribute = $object->getAttribute($attribute_name)) {
-                $class = !empty($object->getMetadata()['attributes'][$attribute_name]) ? $object->getMetadata()['attributes'][$attribute_name]['class'] : false;
-                if ($class && class_exists($class)) {
-                    $object->addAttribute($attribute_name, $values['type'], null, CDFObject::LANGUAGE_UNDETERMINED, $class);
-                } else {
-                    $object->addAttribute($attribute_name, $values['type'], null);
-                }
-                $attribute = $object->getAttribute($attribute_name);
-            }
-            $value_property = (new \ReflectionClass($attribute))->getProperty('value');
-            $value_property->setAccessible(true);
-            $value_property->setValue($attribute, $values['value']);
-        }
-        $event->setObject($object);
+        $event->setObject(CDFObject::fromArray($event->getData()));
         $event->stopPropagation();
     }
 }
