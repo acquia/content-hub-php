d61a3986b3f4cf962012e74cf5b3ec99c2a642f0
Eugene Yurkov <y.yurkov@svitla.com>
2019-04-25 18:53:53 +0300

    Add testDeleteClient. Move exceptions handling into the comments.

--- test/ContentHubClientTest.php
+++ test/ContentHubClientTest.php
@@ -5,6 +5,7 @@ namespace Acquia\ContentHubClient\test;
 
 use Acquia\ContentHubClient\CDF\CDFObject;
 use Acquia\ContentHubClient\ContentHubClient;
+use Acquia\ContentHubClient\Settings;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Psr7\Response;
 use PHPUnit\Framework\TestCase;
@@ -43,6 +44,9 @@ class ContentHubClientTest extends TestCase
     gc_collect_cycles();
   }
 
+  /**
+   * @throws GuzzleException
+   */
   public function testDefinition()
   {
     $jsonResponses = $this->getJsonResponses();
@@ -53,13 +57,13 @@ class ContentHubClientTest extends TestCase
     $this->contentHubClient->shouldReceive('request')
       ->andReturn($responseMock);
 
-    try {
-      $this->assertEquals($jsonResponses[200], $this->contentHubClient->definition('dummyendpoint'));
-      $this->assertNotEquals($jsonResponses[404], $this->contentHubClient->definition('dummyendpoint'));
-    } catch (GuzzleException $exception) {
-    }
+    $this->assertEquals($jsonResponses[200], $this->contentHubClient->definition('dummyendpoint'));
+    $this->assertNotEquals($jsonResponses[404], $this->contentHubClient->definition('dummyendpoint'));
   }
 
+  /**
+   * @throws GuzzleException
+   */
   public function testPutEntity()
   {
     $cdfObjectMock = $this->getCdfObjectMock();
@@ -69,13 +73,13 @@ class ContentHubClientTest extends TestCase
     $this->contentHubClient->shouldReceive('put')
       ->andReturnValues([$jsonResponses[200], $jsonResponses[404]]);
 
-    try {
-      $this->assertEquals($jsonResponses[200], $this->contentHubClient->putEntities($cdfObjectMock));
-      $this->assertEquals($jsonResponses[404], $this->contentHubClient->putEntities($cdfObjectMock));
-    } catch (GuzzleException $exception) {
-    }
+    $this->assertEquals($jsonResponses[200], $this->contentHubClient->putEntities($cdfObjectMock));
+    $this->assertEquals($jsonResponses[404], $this->contentHubClient->putEntities($cdfObjectMock));
   }
 
+  /**
+   * @throws GuzzleException
+   */
   public function testPutEntities()
   {
     $cdfObjectMock = $this->getCdfObjectMock();
@@ -84,13 +88,13 @@ class ContentHubClientTest extends TestCase
     $this->contentHubClient->shouldReceive('put')
       ->andReturnValues([$jsonResponses[200], $jsonResponses[404]]);
 
-    try {
-      $this->assertEquals($jsonResponses[200], $this->contentHubClient->putEntity($cdfObjectMock));
-      $this->assertEquals($jsonResponses[404], $this->contentHubClient->putEntity($cdfObjectMock));
-    } catch (GuzzleException $exception) {
-    }
+    $this->assertEquals($jsonResponses[200], $this->contentHubClient->putEntity($cdfObjectMock));
+    $this->assertEquals($jsonResponses[404], $this->contentHubClient->putEntity($cdfObjectMock));
   }
 
+  /**
+   * @throws GuzzleException
+   */
   public function testPostEntities()
   {
     $cdfObjectMock = $this->getCdfObjectMock();
@@ -99,13 +103,13 @@ class ContentHubClientTest extends TestCase
     $this->contentHubClient->shouldReceive('post')
       ->andReturnValues([$jsonResponses[200], $jsonResponses[404]]);
 
-    try {
-      $this->assertEquals($jsonResponses[200], $this->contentHubClient->postEntities($cdfObjectMock));
-      $this->assertEquals($jsonResponses[404], $this->contentHubClient->postEntities($cdfObjectMock));
-    } catch (GuzzleException $exception) {
-    }
+    $this->assertEquals($jsonResponses[200], $this->contentHubClient->postEntities($cdfObjectMock));
+    $this->assertEquals($jsonResponses[404], $this->contentHubClient->postEntities($cdfObjectMock));
   }
 
+  /**
+   * @throws GuzzleException
+   */
   public function testDeleteEntity()
   {
     $uuid = '11111111-0000-0000-0000-000000000000';
@@ -114,13 +118,13 @@ class ContentHubClientTest extends TestCase
     $this->contentHubClient->shouldReceive('delete')
       ->andReturnValues([$jsonResponses[200], $jsonResponses[404]]);
 
-    try {
-      $this->assertEquals($jsonResponses[200], $this->contentHubClient->deleteEntity($uuid));
-      $this->assertEquals($jsonResponses[404], $this->contentHubClient->deleteEntity($uuid));
-    } catch (GuzzleException $exception) {
-    }
+    $this->assertEquals($jsonResponses[200], $this->contentHubClient->deleteEntity($uuid));
+    $this->assertEquals($jsonResponses[404], $this->contentHubClient->deleteEntity($uuid));
   }
 
+  /**
+   * @throws GuzzleException
+   */
   public function testDeleteInterest()
   {
     $uuid = '11111111-0000-0000-0000-000000000000';
@@ -130,13 +134,14 @@ class ContentHubClientTest extends TestCase
     $this->contentHubClient->shouldReceive('delete')
       ->andReturnValues([$jsonResponses[200], $jsonResponses[404]]);
 
-    try {
-      $this->assertEquals($jsonResponses[200], $this->contentHubClient->deleteInterest($uuid, $webhookUuid));
-      $this->assertEquals($jsonResponses[404], $this->contentHubClient->deleteInterest($uuid, $webhookUuid));
-    } catch (GuzzleException $exception) {
-    }
+    $this->assertEquals($jsonResponses[200], $this->contentHubClient->deleteInterest($uuid, $webhookUuid));
+    $this->assertEquals($jsonResponses[404], $this->contentHubClient->deleteInterest($uuid, $webhookUuid));
   }
 
+  /**
+   * @throws GuzzleException
+   * @throws \Exception
+   */
   public function testPurge()
   {
     $jsonResponses = $this->getJsonResponses();
@@ -147,14 +152,14 @@ class ContentHubClientTest extends TestCase
     $this->contentHubClient->shouldReceive('getResponseJson')
       ->andReturnValues([$jsonResponses[200], $jsonResponses[404]]);
 
-    try {
-      $this->assertEquals($jsonResponses[200], $this->contentHubClient->purge());
-      $this->assertEquals($jsonResponses[404], $this->contentHubClient->purge());
-    } catch (GuzzleException $exception) {
-    } catch (\Exception $exception) {
-    }
+    $this->assertEquals($jsonResponses[200], $this->contentHubClient->purge());
+    $this->assertEquals($jsonResponses[404], $this->contentHubClient->purge());
   }
 
+  /**
+   * @throws GuzzleException
+   * @throws \Exception
+   */
   public function testRestore()
   {
     $jsonResponses = $this->getJsonResponses();
@@ -165,14 +170,14 @@ class ContentHubClientTest extends TestCase
     $this->contentHubClient->shouldReceive('getResponseJson')
       ->andReturnValues([$jsonResponses[200], $jsonResponses[404]]);
 
-    try {
-      $this->assertEquals($jsonResponses[200], $this->contentHubClient->restore());
-      $this->assertEquals($jsonResponses[404], $this->contentHubClient->restore());
-    } catch (GuzzleException $exception) {
-    } catch (\Exception $exception) {
-    }
+    $this->assertEquals($jsonResponses[200], $this->contentHubClient->restore());
+    $this->assertEquals($jsonResponses[404], $this->contentHubClient->restore());
   }
 
+  /**
+   * @throws GuzzleException
+   * @throws \Exception
+   */
   public function testReindex()
   {
     $jsonResponses = $this->getJsonResponses();
@@ -183,14 +188,14 @@ class ContentHubClientTest extends TestCase
     $this->contentHubClient->shouldReceive('getResponseJson')
       ->andReturnValues([$jsonResponses[200], $jsonResponses[404]]);
 
-    try {
-      $this->assertEquals($jsonResponses[200], $this->contentHubClient->reindex());
-      $this->assertEquals($jsonResponses[404], $this->contentHubClient->reindex());
-    } catch (GuzzleException $exception) {
-    } catch (\Exception $exception) {
-    }
+    $this->assertEquals($jsonResponses[200], $this->contentHubClient->reindex());
+    $this->assertEquals($jsonResponses[404], $this->contentHubClient->reindex());
   }
 
+  /**
+   * @throws GuzzleException
+   * @throws \Exception
+   */
   public function testGetResponseJson()
   {
     $jsonResponses = $this->getJsonResponses();
@@ -198,29 +203,61 @@ class ContentHubClientTest extends TestCase
     $responseMock->shouldReceive('getBody')
       ->andReturnValues([$jsonResponses[200], $jsonResponses[404]]);
 
-    try {
-      $this->assertEquals($jsonResponses[200], ContentHubClient::getResponseJson($responseMock));
-      $this->assertEquals($jsonResponses[404], ContentHubClient::getResponseJson($responseMock));
-    } catch (GuzzleException $exception) {
-    } catch (\Exception $exception) {
-    }
+    $this->assertEquals($jsonResponses[200], ContentHubClient::getResponseJson($responseMock));
+    $this->assertEquals($jsonResponses[404], ContentHubClient::getResponseJson($responseMock));
   }
 
+  /**
+   * @throws GuzzleException
+   * @throws \Exception
+   */
+  public function testDeleteClient()
+  {
+    $jsonResponses = $this->getJsonResponses();
+    $uuid = '11111111-0000-0000-0000-000000000000';
+
+    $settingsMock = $this->getMockBuilder(Settings::class)
+      ->disableOriginalConstructor()
+      ->setMethods(['getUuid'])
+      ->getMock();
+
+    $settingsMock->expects($this->any())
+      ->method('getUuid')
+      ->willReturn($uuid);
+
+    $this->contentHubClient->shouldReceive('getSettings')
+      ->andReturn($settingsMock);
+
+    $this->contentHubClient->shouldReceive('deleteEntity')
+      ->andReturnValues([true, true, false]);
+
+    $this->contentHubClient->shouldReceive('delete')
+      ->andReturnValues([$jsonResponses[200], $jsonResponses[404], $jsonResponses[200]]);
+
+    $this->assertEquals($jsonResponses[200], $this->contentHubClient->deleteClient($uuid));
+    $this->assertEquals($jsonResponses[404], $this->contentHubClient->deleteClient($uuid));
+    $this->expectException(\Exception::class);
+    $this->contentHubClient->deleteClient($uuid);
+  }
+
+  /**
+   * @return array
+   */
   public function getJsonResponses()
   {
-    $successJsonResponse = \GuzzleHttp\json_encode([
-      'response_code' => 200,
-    ]);
-    $failureJsonResponse = \GuzzleHttp\json_encode([
-      'response_code' => 404,
-    ]);
-
     return [
-      200 => $successJsonResponse,
-      404 => $failureJsonResponse,
+      200 => \GuzzleHttp\json_encode([
+        'response_code' => 200,
+      ]),
+      404 => \GuzzleHttp\json_encode([
+        'response_code' => 404,
+      ]),
     ];
   }
 
+  /**
+   * @return mixed
+   */
   public function getCdfObjectMock()
   {
     $cdfObjectMock = $this->getMockBuilder(CDFObject::class)
