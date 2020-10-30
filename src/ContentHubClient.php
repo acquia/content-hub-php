99c8e2abf59222bb70e9aee1bdc741e25bfad94d
Attila Tilman <attila.tilman@cheppers.com>
2020-10-30 13:57:19 +0100

    LCH-4545: Add webhook status function

--- src/ContentHubClient.php
+++ src/ContentHubClient.php
@@ -689,6 +689,18 @@ class ContentHubClient extends Client {
   }
 
   /**
+   * Returns webhooks status information.
+   *
+   * @return array
+   *   Webhooks status information.
+   *
+   * @throws \Exception
+   */
+  public function getWebhookStatus() {
+    return self::getResponseJson($this->get('settings/webhooks/status'));
+  }
+
+  /**
    * Returns interests list.
    *
    * @param string $webhook_uuid
