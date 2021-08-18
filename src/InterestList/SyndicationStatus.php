39f06e8aa5a75077f29ebf0d78e590be82d435dd
Peter <peter.pajor@acquia.com>
2021-08-18 15:07:50 +0200

    LCH-5024: Implement new interest list endpoint. (#123)

---
+++ src/InterestList/SyndicationStatus.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Acquia\ContentHubClient\InterestList;
+
+/**
+ * Represents possible syndication statuses.
+ *
+ * @package Acquia\ContentHubClient\InterestList
+ */
+final class SyndicationStatus {
+
+  public const UNKNOWN = 'UNKNOWN';
+  public const READY_FOR_SYNDICATION = 'READY-FOR-SYNDICATION';
+  public const SYNDICATION_IN_PROGRESS = 'SYNDICATION-IN-PROGRESS';
+  public const SYNDICATION_FAILED = 'SYNDICATION-FAILED';
+  public const SYNDICATION_SUCCESSFUL = 'SYNDICATION-SUCCESSFUL';
+  public const QUEUED_TO_EXPORT = 'QUEUED-TO-EXPORT';
+  public const EXPORT_SUCCESSFUL = 'EXPORT-SUCCESSFUL';
+  public const EXPORT_FAILED = 'EXPORT-FAILED';
+  public const IMPORT_SUCCESSFUL = 'IMPORT-SUCCESSFUL';
+  public const IMPORT_FAILED = 'IMPORT-FAILED';
+
+}
