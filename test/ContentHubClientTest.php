ad147afd3a73cee003f2f9bfe70442613778bc3d
ankitsingh0188 <ankitsingh0188@gmail.com>
2021-09-06 16:49:30 +0530

    2.x-dashboard: Trigger the packagist build again

--- test/ContentHubClientTest.php
+++ test/ContentHubClientTest.php
@@ -39,7 +39,7 @@ use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 class ContentHubClientTest extends TestCase {
 
   /**
-   * CH client.
+   * Content Hub client.
    *
    * @var \Acquia\ContentHubClient\ContentHubClient
    */
