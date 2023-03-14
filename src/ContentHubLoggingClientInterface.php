d8e8f75258f8c3a2e61fd68e3ddace62a72c3478
Peter Pajor <peter.pajor@acquia.com>
2023-03-14 18:55:14 +0100

    LCH-6396: Fix linting issue.

--- src/ContentHubLoggingClientInterface.php
+++ src/ContentHubLoggingClientInterface.php
@@ -13,8 +13,17 @@ interface ContentHubLoggingClientInterface {
    * Sends event logs to event microservice.
    *
    * @param array $logs
-   *   Array of logs. Format:
-   *
+   *   Array of logs. Example:
+   * @code
+   * [
+   *   'object_id' => 'objectid',
+   *   'object_type' => 'objecttype',
+   *   'event_name' => 'anevent',
+   *   'severity' => 'ERROR',
+   *   'content' => 'content',
+   *   'origin' => 'auuid',
+   * ]
+   * @endcode
    *
    * @return array
    *   Response from logging service in an array format.
