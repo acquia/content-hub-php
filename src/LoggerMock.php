531c3c240a9fce008ec31e523c91d4755657c8cd
Narendra Desai <shenvidesainarendra@gmail.com>
2023-05-29 14:54:21 +0530

    LCH-6487: Fixing tests.

--- src/LoggerMock.php
+++ src/LoggerMock.php
@@ -20,7 +20,7 @@ class LoggerMock implements LoggerInterface {
   /**
    * {@inheritDoc}
    */
-  public function log($level, $message, array $context = []): void {
+  public function log($level, string|\Stringable $message, array $context = []): void {
     $log_data['message'] = strip_tags($message);
     $log_data['context'] = $context;
 
@@ -48,49 +48,49 @@ class LoggerMock implements LoggerInterface {
   /**
    * {@inheritDoc}
    */
-  public function emergency($message, array $context = []) {
+  public function emergency(string|\Stringable $message, array $context = []): void {
   }
 
   /**
    * {@inheritDoc}
    */
-  public function alert($message, array $context = []) {
+  public function alert(string|\Stringable $message, array $context = []): void {
   }
 
   /**
    * {@inheritDoc}
    */
-  public function critical($message, array $context = []) {
+  public function critical(string|\Stringable $message, array $context = []): void {
   }
 
   /**
    * {@inheritDoc}
    */
-  public function error($message, array $context = []) {
+  public function error(string|\Stringable $message, array $context = []): void {
   }
 
   /**
    * {@inheritDoc}
    */
-  public function warning($message, array $context = []) {
+  public function warning(string|\Stringable $message, array $context = []): void {
   }
 
   /**
    * {@inheritDoc}
    */
-  public function notice($message, array $context = []) {
+  public function notice(string|\Stringable $message, array $context = []): void {
   }
 
   /**
    * {@inheritDoc}
    */
-  public function info($message, array $context = []) {
+  public function info(string|\Stringable $message, array $context = []): void {
   }
 
   /**
    * {@inheritDoc}
    */
-  public function debug($message, array $context = []) {
+  public function debug(string|\Stringable $message, array $context = []): void {
   }
 
 }
