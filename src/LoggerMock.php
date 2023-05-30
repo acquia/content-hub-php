27940ab9bd8676eaa1f5ce5ca3eb1509b01f308a
Narendra Shenvi Desai <shenvidesainarendra@gmail.com>
2023-05-30 14:21:49 +0530

    LCH-6487: Using PSR logger trait in loggermock. (#179)
    * LCH-6487: Fixing tests.
    
    * LCH-6487: Using logger trait.

--- src/LoggerMock.php
+++ src/LoggerMock.php
@@ -3,12 +3,15 @@
 namespace Acquia\ContentHubClient;
 
 use Psr\Log\LoggerInterface;
+use Psr\Log\LoggerTrait;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Mock Logger created for fetching log messages.
  */
 class LoggerMock implements LoggerInterface {
+
+  use LoggerTrait;
 
   /**
    * Log messages.
@@ -43,54 +46,6 @@ class LoggerMock implements LoggerInterface {
    */
   public function reset(): void {
     $this->logMessages = [];
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public function emergency($message, array $context = []) {
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public function alert($message, array $context = []) {
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public function critical($message, array $context = []) {
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public function error($message, array $context = []) {
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public function warning($message, array $context = []) {
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public function notice($message, array $context = []) {
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public function info($message, array $context = []) {
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public function debug($message, array $context = []) {
   }
 
 }
