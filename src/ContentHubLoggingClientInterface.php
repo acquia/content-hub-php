94f5f0d926f2eebacbb75c284465b242bfe40f61
kirti.garg <kirti.garg@acquia.com>
2023-12-03 13:39:09 +0530

    required interface added

---
+++ src/ContentHubLoggingClientInterface.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Acquia\ContentHubClient;
+
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Defines the behaviour of an event logger client.
+ */
+interface ContentHubLoggingClientInterface {
+
+  /**
+   * Sends event logs to event microservice.
+   *
+   * @param array $logs
+   *   Array of logs. Example:
+   *
+   * @code
+   *   [
+   *     'object_id' => 'objectid',
+   *     'object_type' => 'objecttype',
+   *     'event_name' => 'anevent',
+   *     'severity' => 'ERROR',
+   *     'content' => 'content',
+   *     'origin' => 'auuid',
+   *   ]
+   * @endcode
+   *
+   * @return array
+   *   Response from logging service in an array format.
+   *
+   * @throws \Exception
+   */
+  public function sendLogs(array $logs): array;
+
+  /**
+   * Pings the service to make sure it is available.
+   *
+   * @return \Psr\Http\Message\ResponseInterface
+   *   The response in array format or null.
+   */
+  public function ping(): ResponseInterface;
+
+}
