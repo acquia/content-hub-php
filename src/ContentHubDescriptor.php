52f04f8d25015182756f4a8a4751d461700ab304
kirti.garg <kirti.garg@acquia.com>
2023-12-03 14:10:05 +0530

    src/ContentHubDescriptor.php added

---
+++ src/ContentHubDescriptor.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Acquia\ContentHubClient;
+
+use function GuzzleHttp\default_user_agent;
+
+/**
+ * Content Hub Descriptor provides Version and user agent string.
+ */
+final class ContentHubDescriptor {
+
+  /**
+   * Library version for client.
+   */
+  public const LIB_VERSION = '3.x-dev';
+
+  /**
+   * Library name for client.
+   */
+  public const LIBRARYNAME = 'AcquiaContentHubPHPLib';
+
+  /**
+   * Returns default user agent string.
+   *
+   * @return string
+   *   User agent string.
+   */
+  public static function userAgent(): string {
+    return self::LIBRARYNAME . '/' . self::LIB_VERSION . ' ' . default_user_agent();
+  }
+
+}
