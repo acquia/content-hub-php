6ac3d7bb5b95c6cb45609dffd1500170aa17fa02
Kirti Garg <kirti.garg@acquia.com>
2023-12-04 18:27:02 +0530

    LCH 6790 3.4.x : Port maintenance mode changes (#188)

---
+++ src/StatusCodes.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Acquia\ContentHubClient;
+
+/**
+ * Contains status codes returned by the service.
+ */
+final class StatusCodes {
+
+  /**
+   * Returned when the service is intentionally configured to be in maintenance.
+   */
+  public const SERVICE_UNDER_MAINTENANCE = 503;
+
+}
