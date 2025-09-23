201f7303f7c25c0b55e65fcf1ab5ddc8130fe340
kirti.garg <kirti.garg@acquia.com>
2025-09-23 11:53:34 +0530

    CHS-8837 | syndication_mode added to ClientMetadata

--- test/MetaData/ClientMetaDataTest.php
+++ test/MetaData/ClientMetaDataTest.php
@@ -22,6 +22,7 @@ class ClientMetaDataTest extends TestCase {
     'is_publisher' => TRUE,
     'is_subscriber' => FALSE,
     'webhook_version' => '2.0',
+    'syndication_mode' => 'push',
     'config' => [
       'valid_ssl' => TRUE,
       'drupal_version' => '10.1.1',
@@ -42,7 +43,7 @@ class ClientMetaDataTest extends TestCase {
    * @covers ::toArray
    */
   public function testGetMetaData(): void {
-    $this->sut = new ClientMetaData($this->metadata['client_type'], $this->metadata['is_publisher'], $this->metadata['is_subscriber'], $this->metadata['webhook_version'], $this->metadata['config']);
+    $this->sut = new ClientMetaData($this->metadata['client_type'], $this->metadata['is_publisher'], $this->metadata['is_subscriber'], $this->metadata['webhook_version'], $this->metadata['syndication_mode'], $this->metadata['config']);
     $client_metadata = $this->sut->toArray();
     $this->assertEquals($this->metadata, $client_metadata);
   }
