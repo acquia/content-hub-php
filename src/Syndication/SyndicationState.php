538de64958608c1f80c00637df3e1edd0c507c56
Kirti Garg <kirti.garg@acquia.com>
2025-11-07 18:39:40 +0530

    CHS-8847 | Implement drush ach-qs:update Command (#223)
    * CHS-8847 | Implement drush ach-qs:update Command
    
    * minor fixes
    
    * Funcation name updated

--- src/Syndication/SyndicationState.php
+++ src/Syndication/SyndicationState.php
@@ -15,4 +15,34 @@ final class SyndicationState {
   public const PROCESSED = 'processed';
   public const PENDING = 'pending';
 
+  /**
+   * Validates syndication state.
+   *
+   * @param string $state
+   *   The state to validate.
+   *
+   * @return bool
+   *   TRUE if state is valid, FALSE otherwise.
+   */
+  public static function isValidState(string $state): bool {
+    $states = SyndicationState::getAll();
+    return in_array($state, $states, TRUE);
+  }
+
+  /**
+   * Returns syndication states.
+   *
+   * @return array
+   *   Returns array of valid syndication states.
+   */
+  public static function getAll(): array {
+    return [
+      self::PROCESSING,
+      self::FAILED,
+      self::QUEUED,
+      self::PROCESSED,
+      self::PENDING,
+    ];
+  }
+
 }
