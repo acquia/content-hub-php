00b9d756a55d3c906c532f8b9c94599e2f8bb531
kirti.garg <kirti.garg@acquia.com>
2025-11-18 12:38:33 +0530

    spacing fix

--- src/ContentHubClient.php
+++ src/ContentHubClient.php
@@ -1779,7 +1779,7 @@ class ContentHubClient implements ClientInterface {
    * Creates a new syndication queue item.
    *
    * @param array $data
-   *   [
+   *   Eg : [
    *     'client_uuid' => '1c6f7a8a-5cf6-4bb2-8e09-e64c0d63629f',
    *     'entity_uuid' => '0c6f7a8a-5cf6-4bb2-8e09-e64c0d63629f',
    *     'payload' => [
